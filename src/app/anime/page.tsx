644b53e4b91c8dc92572fb8d81a68204c2ab9445
Ezii54 <alunheadshoter@gmail.com>
2024-07-25 02:53:24 +0700

    REDEPLOY YG KE 985713

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,4 +1,4 @@
-import ListAnime from "@/app/anime/components/ListAnime";
+//import ListAnime from "@/app/anime/components/ListAnime";
 import { getResAnimeAPI, getResListAnimeAPI, reproduce } from "@/libs/api";
 import Search from "./components/Search";
 import ListGenre from "./components/ListGenre";
@@ -6,12 +6,11 @@ import ListLetter from "./components/ListLetter";
 
 const Page = async () => {
    const limitAnime = 6;
-   const popAnime = await getResAnimeAPI("/top/anime", `limit=${limitAnime}`);
-   let recAnime = await getResListAnimeAPI("/recommendations/anime", "entry");
-   recAnime = await reproduce(recAnime, limitAnime);
+   //const popAnime = await getResAnimeAPI("/top/anime", `limit=${limitAnime}`);
+   //let recAnime = await getResListAnimeAPI("/recommendations/anime", "entry");
+   //recAnime = await reproduce(recAnime, limitAnime);
 
    const genreAnime = await getResAnimeAPI("/genres/anime", "filter=genres");
-   const themeAnime = await getResAnimeAPI("/genres/anime", "filter=themes");
    const demographicsAnime = await getResAnimeAPI(
       "/genres/anime",
       "filter=demographics"
@@ -22,15 +21,17 @@ const Page = async () => {
          <section>
             <Search />
          </section>
-
+         <hr className="text-color-dark text-opacity-80 mt-1" />
          <section>
             <ListLetter />
          </section>
          <hr className="text-color-dark text-opacity-80 mt-1" />
          <section>
             <ListGenre api={genreAnime} title={"Genres"} />
-            <ListGenre api={themeAnime} title={"Themes"} />
-            <ListGenre api={demographicsAnime} title={"Demographics"} />
+            <ListGenre
+               api={await getResAnimeAPI("/genres/anime", "filter=themes")}
+               title={"Themes"}
+            />
          </section>
       </div>
    );
