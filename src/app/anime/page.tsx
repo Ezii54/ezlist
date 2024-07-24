a8bfe10088dcb821cd353ceca098cc197961a8d4
Ezii54 <alunheadshoter@gmail.com>
2024-07-25 02:58:06 +0700

    TESTING YG KE 285963265

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,4 +1,4 @@
-//import ListAnime from "@/app/anime/components/ListAnime";
+import ListAnime from "@/app/anime/components/ListAnime";
 import { getResAnimeAPI, getResListAnimeAPI, reproduce } from "@/libs/api";
 import Search from "./components/Search";
 import ListGenre from "./components/ListGenre";
@@ -6,15 +6,9 @@ import ListLetter from "./components/ListLetter";
 
 const Page = async () => {
    const limitAnime = 6;
-   //const popAnime = await getResAnimeAPI("/top/anime", `limit=${limitAnime}`);
-   //let recAnime = await getResListAnimeAPI("/recommendations/anime", "entry");
-   //recAnime = await reproduce(recAnime, limitAnime);
-
-   const genreAnime = await getResAnimeAPI("/genres/anime", "filter=genres");
-   const demographicsAnime = await getResAnimeAPI(
-      "/genres/anime",
-      "filter=demographics"
-   );
+   const popAnime = await getResAnimeAPI("/top/anime", `limit=${limitAnime}`);
+   let recAnime = await getResListAnimeAPI("/recommendations/anime", "entry");
+   recAnime = await reproduce(recAnime, limitAnime);
 
    return (
       <div className="p-1">
@@ -23,14 +17,42 @@ const Page = async () => {
          </section>
          <hr className="text-color-dark text-opacity-80 mt-1" />
          <section>
+            <ListAnime
+               api={popAnime}
+               title="Most Popular"
+               linkHref="/anime/res/viewmore"
+               linkTitle="(View More)"
+            />
+         </section>
+         <hr className="text-color-dark text-opacity-80 mt-1" />
+         <section>
+            <ListAnime
+               api={recAnime}
+               title="Recommendations"
+               linkHref=""
+               linkTitle=""
+            />
+         </section>
+         <hr className="text-color-dark text-opacity-80 mt-1" />
+         <section>
             <ListLetter />
          </section>
          <hr className="text-color-dark text-opacity-80 mt-1" />
          <section>
-            <ListGenre api={genreAnime} title={"Genres"} />
+            <ListGenre
+               api={await getResAnimeAPI("/genres/anime", "filter=genres")}
+               title={"Genres:"}
+            />
             <ListGenre
                api={await getResAnimeAPI("/genres/anime", "filter=themes")}
-               title={"Themes"}
+               title={"Themes:"}
+            />
+            <ListGenre
+               api={await getResAnimeAPI(
+                  "/genres/anime",
+                  "filter=demographics"
+               )}
+               title={"Demographics:"}
             />
          </section>
       </div>
