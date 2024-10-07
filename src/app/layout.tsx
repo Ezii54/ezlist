9a03acfd7512f81d5de814b93c58e608a340b0b0
Ezii54 <alunheadshoter@gmail.com>
2024-10-08 03:22:14 +0700

    add some group chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = localFont({
-   src: "./fonts/GeistVF.woff",
-   variable: "--font-geist-sans",
-   weight: "100 900",
-});
-const geistMono = localFont({
-   src: "./fonts/GeistMonoVF.woff",
-   variable: "--font-geist-mono",
-   weight: "100 900",
-});
+import { Andika } from "next/font/google";
+const font = Andika({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
    title: "ezList",
@@ -28,7 +19,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased bg-color-lg text-color-white p-1`}
+            className={`${font.className} antialiased bg-color-lg text-color-white p-1`}
          >
             <Navbar />
             {children}
